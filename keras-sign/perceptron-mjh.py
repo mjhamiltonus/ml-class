05b3e4da6f66e68221a5efabadb3650daf293072
Michael Hamilton <mjhamiltonus@gmail.com>
2018-12-03 18:31:10 +0000

    add the softmax

--- keras-sign/perceptron-mjh.py
+++ keras-sign/perceptron-mjh.py
@@ -32,39 +32,42 @@ y_test = np_utils.to_categorical(y_test)
 
 num_classes = y_train.shape[1]
 
+print("y_test shape: {:}".format(y_test.shape))
+
 # you may want to normalize the data here..
 
-# create model
-# model=Sequential()
-# model.add(Flatten(input_shape=(img_width, img_height)))
-# model.add(Dense(num_classes))
-# model.compile(loss=config.loss, optimizer=config.optimizer,
-#                 metrics=['accuracy'])
 
+
+# create model
+model=Sequential()
+model.add(Flatten(input_shape=(img_width, img_height)))
+model.add(Dense(num_classes), activation='softmax')
 
 # MJH  create model used in the "fashion" classifier
-config.first_layer_convs = 32
-config.first_layer_conv_width = 3
-config.first_layer_conv_height = 3
+# config.first_layer_convs = 32
+# config.first_layer_conv_width = 3
+# config.first_layer_conv_height = 3
 
-model = Sequential()
-model.add(Reshape((img_width, img_height, 1), input_shape=(img_width,img_height)))
-model.add(Conv2D(32,
-    (config.first_layer_conv_width, config.first_layer_conv_height),
-    input_shape=(img_width, img_height, 1),
-    activation='relu'))
-# ^^^ MJH
-model.add(Flatten(input_shape=(img_width, img_height)))
-model.add(Dropout(config.dropout))
-model.add(Dense(config.hidden_layer_1_size, activation='relu'))
-model.add(Dropout(config.dropout))
-model.add(Dense(num_classes, activation='softmax'))
+# model = Sequential()
+# model.add(Reshape((img_width, img_height, 1), input_shape=(img_width,img_height)))
+# model.add(Conv2D(32,
+#     (config.first_layer_conv_width, config.first_layer_conv_height),
+#     input_shape=(img_width, img_height, 1),
+#     activation='relu'))
+
+# model.add(Flatten(input_shape=(img_width, img_height)))
+# model.add(Dropout(config.dropout))
+# model.add(Dense(config.hidden_layer_1_size, activation='relu'))
+# model.add(Dropout(config.dropout))
+# model.add(Dense(num_classes, activation='softmax'))
 model.compile(loss=config.loss, optimizer=config.optimizer,
               metrics=['accuracy'])
-# Fit the model
-# model.fit(X_train, y_train, epochs=config.epochs, validation_data=(X_test, y_test),
-#           callbacks=[WandbCallback(data_type="image", labels=labels)])
 
 
 # Fit the model
-model.fit(X_train, y_train, epochs=config.epochs, validation_data=(X_test, y_test), callbacks=[WandbCallback(data_type="image", labels=signdata.letters)])
+model.fit(X_train[:100], y_train[:100], epochs=config.epochs, 
+          # validation_data=(X_test, y_test), 
+          callbacks=[WandbCallback(data_type="image", labels=signdata.letters)])
+
+print("Target:      \n{:}".format(y_train[:2]))
+print("Predictions: \n{:}".format(model.predict(X_train[:2])))
