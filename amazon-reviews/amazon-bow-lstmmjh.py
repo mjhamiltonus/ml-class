8ca41ae335c5d8aaad160c8dcf95f54eac0ee937
Michael Hamilton <mjhamiltonus@gmail.com>
2018-12-04 19:46:36 +0000

    Initial LSTM version

--- amazon-reviews/amazon-bow-lstmmjh.py
+++ amazon-reviews/amazon-bow-lstmmjh.py
@@ -18,41 +18,62 @@ config.filters = 250
 config.kernel_size = 3
 config.hidden_dims = 250
 config.epochs = 10
+config.batch_size = 32
 
 (train_summary, train_review_text, train_labels), (test_summary, test_review_text, test_labels) = amazon.load_amazon()
 
+# Need to do the tokenization 
 tokenizer = text.Tokenizer(num_words=config.vocab_size)
 tokenizer.fit_on_texts(train_review_text)
-X_train = tokenizer.texts_to_matrix(train_review_text)
-X_test = tokenizer.texts_to_matrix(test_review_text)
+
+### vvvvv  changes for LSTM  vvvvv
+X_train = tokenizer.texts_to_sequences(X_train)
+X_test = tokenizer.texts_to_sequences(X_test)
+
+X_train = sequence.pad_sequences(X_train, maxlen=config.maxlen)
+X_test = sequence.pad_sequences(X_test, maxlen=config.maxlen)
+
+embeddings_index = dict()
+f = open('glove.6B.100d.txt')
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+embedding_matrix = np.zeros((config.vocab_size, 100))
+for word, index in tokenizer.word_index.items():
+    if index > config.vocab_size - 1:
+        break
+    else:
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[index] = embedding_vector
+### ^^^^^^ changes for LSTM ^^^^^^^^
+### old stuff from BOW
+# X_train = tokenizer.texts_to_matrix(train_review_text)
+# X_test = tokenizer.texts_to_matrix(test_review_text)
+### old stuff 
 
 y_train = train_labels
 y_test = test_labels
 
-# MJH - CNN model from imdb-cnn.py
+# MJH - LSTM model from imdb-embedding-bidir-mjh.py
 model = Sequential()
-model.add(Embedding(config.vocab_size,
-                    config.embedding_dims,
-                    input_length=config.maxlen))
-model.add(Dropout(0.5))
-model.add(Conv1D(config.filters,
-                 config.kernel_size,
-                 padding='valid',
-                 activation='relu'))
-model.add(Flatten())
-model.add(Dense(config.hidden_dims, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Embedding(config.vocab_size, 100, input_length=config.maxlen, weights=[embedding_matrix], trainable=False))
+# 
+model.add(Bidirectional(LSTM(50, activation="sigmoid", dropout=0.50, recurrent_dropout=0.50)))
 
+model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy',
-              optimizer='adam',
+              optimizer='rmsprop',
               metrics=['accuracy'])
 
 model.fit(X_train, y_train,
           batch_size=config.batch_size,
           epochs=config.epochs,
           validation_data=(X_test, y_test), callbacks=[WandbCallback()])
-
 
 
 
