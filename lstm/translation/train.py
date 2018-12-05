cf4cbbba03f8d44d9b375b0b384f67a2a1737d83
Michael Hamilton <mjhamiltonus@gmail.com>
2018-12-05 00:02:38 +0000

    change to CuDNNLSTM - 3s vs. 14s

--- lstm/translation/train.py
+++ lstm/translation/train.py
@@ -14,7 +14,7 @@ http://www.manythings.org/anki/
 from __future__ import print_function
 
 from keras.models import Model
-from keras.layers import Input, LSTM, Dense
+from keras.layers import Input, LSTM, Dense, CuDNNLSTM
 import wandb
 from wandb.keras import WandbCallback
 
@@ -24,7 +24,7 @@ wandb.init()
 config = wandb.config
 
 config.batch_size = 64  # Batch size for training.
-config.epochs = 5  # Number of epochs to train for.
+config.epochs = 100  # Number of epochs to train for.
 config.latent_dim = 256  # Latent dimensionality of the encoding space.
 config.num_samples = 10000  # Number of samples to train on.
 config.data_path = 'deu.txt' # Path to the data txt file on disk.
@@ -92,7 +92,7 @@ for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
 
 # Define an input sequence and process it.
 encoder_inputs = Input(shape=(None, num_encoder_tokens))
-encoder = LSTM(config.latent_dim, return_state=True)
+encoder = CuDNNLSTM(config.latent_dim, return_state=True)
 encoder_outputs, state_h, state_c = encoder(encoder_inputs)
 # We discard `encoder_outputs` and only keep the states.
 encoder_states = [state_h, state_c]
@@ -102,7 +102,7 @@ decoder_inputs = Input(shape=(None, num_decoder_tokens))
 # We set up our decoder to return full output sequences,
 # and to return internal states as well. We don't use the
 # return states in the training model, but we will use them in inference.
-decoder_lstm = LSTM(config.latent_dim, return_sequences=True, return_state=True)
+decoder_lstm = CuDNNLSTM(config.latent_dim, return_sequences=True, return_state=True)
 decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
                                      initial_state=encoder_states)
 decoder_dense = Dense(num_decoder_tokens, activation='softmax')
